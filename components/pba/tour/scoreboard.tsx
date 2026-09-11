813c56ee9dd5e3e9e2599bf280b57f2732d28a8d
syauri-bit <syauri@gmail.com>
2026-09-11 22:07:05 +0900

    Fix import path for useTheme in scoreboard.tsx

--- components/pba/tour/scoreboard.tsx
+++ components/pba/tour/scoreboard.tsx
@@ -6,7 +6,7 @@ import type { MatchConfig } from "@/lib/pba/types"
 import { TEAM_COLORS } from "@/lib/pba/types"
 import { isDoublesSet, getSetPlayers } from "@/lib/pba/game"
 import { withAlpha } from "@/lib/pba/colors"
-import { useTheme } from "./theme-context"
+import { useTheme } from "../theme-context"
 
 interface ScoreboardProps {
   config: MatchConfig
